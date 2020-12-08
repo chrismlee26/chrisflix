e62409422da9cc8fb1ea520ad79cbc62038e7b46
CHRIS_LEE <chrismlee87@gmail.com>
2020-12-08 02:23:17 -0800

    rows fixed, posters fixed, more css

--- src/App.js
+++ src/App.js
@@ -5,10 +5,32 @@ import requests from './requests';
 
 function App() {
   return (
-    <div className="App">
-      <h1>sup</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    <div className="app">
+      <Row title="NETFLIX ORIGINALS" 
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
+      />
+      <Row title="Trending Now" 
+      fetchUrl={requests.fetchTrending} 
+      />
+      <Row title="Top Rated" 
+      fetchUrl={requests.fetchTopRated} 
+      />
+      <Row title="Action Movies" 
+      fetchUrl={requests.fetchActionMovies} 
+      />
+      <Row title="Comedy Movies" 
+      fetchUrl={requests.fetchComedyMovies} 
+      />
+      <Row title="Horror Movies" 
+      fetchUrl={requests.fetchHorrorMovies} 
+      />
+      <Row title="Romance Movies" 
+      fetchUrl={requests.fetchRomanceMovies} 
+      />
+      <Row title="Documentaries" 
+      fetchUrl={requests.fetchDocumentaries} 
+      />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import './Row.css';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -22,13 +22,13 @@ function Row({ title, fetchUrl }) {
   return (
     <div className="row">
       <h2>{title}</h2>
-      
+
       <div className="row__posters">
         {movies.map(movie => (
           <img 
             key={movie.id}
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`} 
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
             alt={movie.name}/>
         ))}
       </div>
