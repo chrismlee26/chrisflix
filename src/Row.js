e12128e2bcc0e119989a79b229a96fd6905a4346
CHRIS_LEE <chrismlee87@gmail.com>
2020-12-08 01:17:20 -0800

    rows working with animations

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
+import './Row.css';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -21,11 +22,14 @@ function Row({ title, fetchUrl }) {
   return (
     <div className="row">
       <h2>{title}</h2>
+      
       <div className="row__posters">
         {movies.map(movie => (
           <img 
+            key={movie.id}
             className="row__poster"
-            src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+            src={`${base_url}${movie.poster_path}`} 
+            alt={movie.name}/>
         ))}
       </div>
     </div>
