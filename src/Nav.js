d32ca1f31fb892b41f4091951f75d6115a26dce8
CHRIS_LEE <chrismlee87@gmail.com>
2020-12-08 16:54:44 -0800

    master - complete

--- src/Nav.js
+++ src/Nav.js
@@ -17,6 +17,10 @@ function Nav() {
 
   return (
     <div className={`nav ${show && "nav__black"}`}>
+      {/* <h1
+        className="nav__logo">
+        CHRISFLIX
+      </h1> */}
       <img
         className="nav__logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
